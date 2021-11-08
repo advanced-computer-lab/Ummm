3df97f4fdce8ae87c01e690fec0fd6537f7fe134
Ahmed Farouk <ahmedfarouk990099@gmail.com>
2021-11-08 18:34:57 +0200

    create flight desgin

--- frontend/src/page/CreateFlight.js
+++ frontend/src/page/CreateFlight.js
@@ -6,9 +6,55 @@ import 'antd/dist/antd.css';
 
 import moment from "moment";
 import {
-  Form,Input,Button,Select,DatePicker,InputNumber} from 'antd';
+  Form,
+  Input,
+  Button,
+  Radio,
+  Select,
+  Cascader,
+  DatePicker,
+  InputNumber,
+  TreeSelect,
+  Switch,
+  message,
+  Mentions
+} from 'antd';
 
 
+  const success = () => {
+    message.success('Flight Successfully Added!');
+  };
+  
+  const warning = () => {
+    message.warning('Fill All Fields Please!');
+  };
+  const warning1 = () => {
+    message.warning('"Flight_No" Must be Filled!');
+  };
+
+  const warning2 = () => {
+    message.warning(' "From" Field Must Exacly be 3 Letters!');
+  };
+
+  const warning3 = () => {
+    message.warning(' "To" Field Must Exacly be 3 Letters!');
+  };
+  const warning4 = () => {
+    message.warning(' "Flight_Date" Must be Filled!');
+  };
+  const warning5 = () => {
+    message.warning(' "Economy_Seats" Must be Filled!');
+  };
+  const warning6 = () => {
+    message.warning(' "Business_Seats" Must be Filled!');
+  };
+  const warning7 = () => {
+    message.warning(' "First_Seats" Must be Filled!');
+  };
+  const warning8 = () => {
+    message.warning('"Terminal" Must be Filled!');
+  };
+  
 const CreateFlight = () => {
   const [componentSize, setComponentSize] = useState('default');
 
@@ -27,12 +73,6 @@ const CreateFlight = () => {
   const onFormLayoutChange = ({ size }) => {
     setComponentSize(size);
   };
-
-  // useEffect(() => {  // Run after page loads "more usefull in get"
-
-  // },[]);
-
-
   const changeHander = (e) => {
     setState( prevData => {
      return {...prevData ,[e.target.name]: e.target.value}})
@@ -41,15 +81,18 @@ const CreateFlight = () => {
 
   const submitHandler = (e) => {
     e.preventDefault();    // prevent reloading the page
-    Object.keys(Data).forEach(key => {
-      if(key=='Economy_Seats' || key=='Business_Seats' || key=='First_Seats')
-      if (Data[key]=="") {
-           Data[key] = 0;
-         }
-       });
+    // Object.keys(Data).forEach(key => {
+    //   if(key=='Economy_Seats' || key=='Business_Seats' || key=='First_Seats')
+    //   if (Data[key]=="") {
+    //        Data[key] = 0;
+    //      }
+    //    });
+    
+
+    if(Data.Flight_No.length==5 &Data.From.length==3 && Data.To.length==3 &&Data.Flight_Date!=null &&Data.Terminal!=''&& Data.Economy_Seats!=''&& Data.First_Seats!=''&& Data.Business_Seats!='' )  {
+      success(); // data succ added less go
     axios.post('http://localhost:8000/createflight',  Data)
     .then(response => {
-      console.log(response);
       setState({
         Flight_No: "",
         From: "",  
@@ -63,6 +106,39 @@ const CreateFlight = () => {
        }).catch(error => {
       console.log(error);
     })
+  }
+
+  else if(Data.Flight_No.length<3  ){
+    warning1();
+  }
+  else if(Data.From.length<3 ){
+    warning2();
+  }
+  else if(Data.To.length<3 ){
+    warning3();
+  }
+  else if(Data.Flight_Date=="" ){
+    warning4();
+  }
+  else if(Data.Terminal=="" ){
+    warning8();
+  }
+  else if(Data.Economy_Seats=='' ){
+    warning5();
+  }
+ 
+  else if(Data.Business_Seats==''){
+    warning6();
+  }
+  else if(Data.First_Seats=='' ){
+    warning7();
+  }
+  else {
+    warning();
+  }
+  
+  
+
 
   };
 
@@ -82,34 +158,68 @@ const CreateFlight = () => {
         }}
         onValuesChange={onFormLayoutChange}
         size={componentSize}
-      >
+      >                     
   
-        <Form.Item label="Flight_No:">
+  <Form.Item  
+        name="Flight_No"
+        rules={[
+          {
+            required: true,
+            message: 'Please Fill it With 2 Letters followed by 3 numbers!',
+          },
+        ]}label="Flight_No">
           <Input type="text" name="Flight_No" maxLength="5" placeholder="FNXXX" value={Data.Flight_No} onChange={(e) => changeHander(e)}/>
         </Form.Item>
         
 
-        <Form.Item label="From">
-          <Input type="text" name="From" maxLength="3" placeholder="3 letters" value={Data.From} onChange={(e) => changeHander(e)}/>
+        <Form.Item  
+        name="From"
+        rules={[
+          {
+            required: true,
+            message: 'Please Fill it With At Least 3 Letters!',
+          },
+        ]}label="From">
+          <Input   type="text" name="From" maxLength="3" placeholder="3 letters"  value={Data.From} onChange={(e) => changeHander(e)} />
         </Form.Item>
 
 
-        <Form.Item label="To">
+        <Form.Item label="To"
+         name="To"
+         rules={[
+           {
+             required: true,
+             message: 'Please Fill it With At Least 3 Letters!',
+           },
+         ]}>
         <Input type="text" name="To" maxLength="3" placeholder="3 letters" value={Data.To} onChange={(e) => changeHander(e)}/>
         </Form.Item>
         
         
 
-        <Form.Item label="Flight Date">
-          <DatePicker type="date" format="DD-MM-YYYY" value={Data.Flight_Date} format="DD-MM-YYYY, HH:mm"
-          showTime="true" disabledDate={d => d.isBefore(new Date())}
+        <Form.Item 
+        name="Flight Date"
+        rules={[
+          {
+            required: true,
+            message: 'Please Select Date!',
+          },
+        ]} label="Flight Date">
+<DatePicker type="date" format="DD-MM-YYYY" disabledDate={d => !d || d.isBefore(new Date())}
            name="FlightDate" onChange={(date) => setState(prevData => {
               return {...prevData ,Flight_Date: date}}) 
     }/>
         </Form.Item>
 
-
-        <Form.Item label="Terminal">
+        <Form.Item 
+        name="Terminal"
+        rules={[
+          {
+            required: true,
+            message: 'Please Select Date!',
+          },
+        ]} label="Terminal">
+      
           <Select type="text" name="Terminal" value={Data.Terminal} onSelect={(value) => setState(prevData => {
     return {...prevData ,Terminal: value}})}>
             <Select.Option value="1">Terminal 1</Select.Option>
@@ -117,20 +227,42 @@ const CreateFlight = () => {
             <Select.Option value="3">Terminal 3</Select.Option>
           </Select>
         </Form.Item>
-                          
-        <Form.Item label="Economy Seats"> 
+
+        <Form.Item 
+        name="Economy Seats"
+        rules={[
+          {
+            required: true,
+            message: 'Please Fill!',
+          },
+        ]} label="Economy Seats">         
           <InputNumber type="Number" name="Economy_Seats" value={Data.Economy_Seats} max={500} placeholder="500 Max" onChange={(number) => setState(prevData => {
               return {...prevData ,Economy_Seats: number}}) 
           }/>
         </Form.Item>
 
-        <Form.Item label="Business Seats"> 
+        
+        <Form.Item 
+        name="Business Seats"
+        rules={[
+          {
+            required: true,
+            message: 'Please Fill!',
+          },
+        ]} label="Business Seats">   
           <InputNumber type="Number" name="Business_Seats" value={Data.Business_Seats} max={500} placeholder="500 Max" onChange={(number) => setState(prevData => {
               return {...prevData ,Business_Seats: number}}) 
           }/>
         </Form.Item>
 
-        <Form.Item label="First Seats"> 
+        <Form.Item 
+        name="First Seats"
+        rules={[
+          {
+            required: true,
+            message: 'Please Fill!',
+          },
+        ]} label="First Seats">   
           <InputNumber type="Number" name="First_Seats" value={Data.First_Seats} max={500} placeholder="500 Max" onChange={(number) => setState(prevData => {
               return {...prevData ,First_Seats: number}}) 
           }/>
