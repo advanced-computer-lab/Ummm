a99d6f13a12976b06a19db125d418ed6d0a536c4
Ahmed Farouk <ahmedfarouk990099@gmail.com>
2021-11-08 22:33:12 +0200

    flighthome

---
+++ frontend/src/page/FlightHome.js
@@ -0,0 +1,60 @@
+
+import './home.scss';
+import './App.css';
+
+import {
+    Form,
+  } from 'antd';
+
+
+
+const FlightHome = () => {
+    // function myFunction() =location.replace("https://www.w3schools.com") ;
+    return (
+        <Form.Item>
+                    {/* <link href="https://fonts.googleapis.com/css?family=Roboto:100" rel="stylesheet"></link>
+            <p id='head1' class='header'>Welcome to </p>
+            <p id='head2' class='header'>Fly Nawwwwww!! </p>
+            <p id='head5' class='header'>Reserve your flights now!!</p> */}
+
+            {/* <form method="POST" action="flighthome.js">  
+            <input type="submit"/>  
+            </form> */}
+        
+  <div class="back">
+    <div class="welcome">Welcome to</div><br></br>
+    <h1>Fly Nawwwwww</h1>
+  </div>
+  <div class="shade"></div>
+  <div>
+  
+  </div>
+
+
+            <a href="LoginPage">  
+
+                    <button2>Admin</button2>
+                </a>
+                
+                <a href="LoginPage"> 
+                    <button3>User</button3>
+                </a>
+  
+                
+  
+
+
+
+  <script>
+       
+</script>
+
+
+
+  </Form.Item>
+       
+    )
+};
+export default FlightHome;
+
+
