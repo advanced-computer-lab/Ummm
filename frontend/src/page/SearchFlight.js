916b994e194ab45b51a15f4f812c5ab09a208e5c
anaselnemr <anasnemr25@gmail.com>
2021-11-08 04:27:14 +0200

    Searching Flights

---
+++ frontend/src/page/SearchFlight.js
@@ -0,0 +1,282 @@
+import { Component, useState,useEffect, useReducer } from 'react';
+import axios from 'axios'
+import ReactDOM from 'react-dom'
+import { useHistory } from 'react-router-dom';
+import 'antd/dist/antd.css'; 
+import './App.css';
+
+import moment from "moment";
+import {
+  Form,
+  Input,
+  Button,
+  Radio,
+  Select,
+  Cascader,
+  DatePicker,
+  InputNumber,
+  TreeSelect,
+  Switch,
+} from 'antd';
+
+
+const SearchFlight = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [componentSize, setComponentSize] = useState('default');
+  const [Result, setResult] = useState();
+  
+  const [Data, setState] = useState({
+    Flight_No: "",
+    From: "",  
+    To: "",
+    Flight_Date: "", // Data type date
+    Terminal: "",
+    Economy_Seats: "",
+    Business_Seats: "",
+    First_Seats: ""
+  });
+  
+
+  const onFormLayoutChange = ({ size }) => {
+    setComponentSize(size);
+  };
+
+  // useEffect(() => {  // Run after page loads "more usefull in get"
+
+  // },[]);
+
+
+  const changeHander = (e) => {
+
+
+    console.log(moment(Data.Flight_Date));
+
+    setState( prevData => {
+     return {...prevData ,[e.target.name]: e.target.value}})
+  };
+
+
+  const searchHandler = (e) => {
+    e.preventDefault(); 
+  
+    const criteria = {};
+    var dd;
+
+    Object.keys(Data).forEach(key => {
+   if (Data[key]!=="") {
+        criteria[key] = Data[key];
+      }
+    });
+    console.log(criteria);
+
+   // prevent reloading the page
+    axios.post('http://localhost:8000/SearchFlight', criteria)
+    .then(response => {
+      setResult(response.data);
+       console.log(Result);
+       setLoading(false);
+      setState({
+        Flight_No: "",
+        From: "",  
+        To: "",
+        Flight_Date: "", // Data type date
+        Terminal: "",
+        Economy_Seats: "",
+        Business_Seats: "",
+        First_Seats: ""
+        })
+       }).catch(error => {
+      console.log(error);
+    })
+
+  };
+
+
+
+  if (isLoading) {
+    return (
+      <>
+        <Form
+          labelCol={{
+            span: 4,
+          }}
+          wrapperCol={{
+            span: 14,
+          }}
+          layout="horizontal"
+          initialValues={{
+            size: componentSize,
+          }}
+          onValuesChange={onFormLayoutChange}
+          size={componentSize}
+        >
+          <Form.Item label="Flight_No:">
+          <Input type="text" name="Flight_No" maxLength="5" placeholder="FNXXX" value={Data.Flight_No} onChange={(e) => changeHander(e)}/>
+        </Form.Item>
+    
+          <Form.Item label="From">
+            <Input type="text" name="From" maxLength="3" placeholder="3 letters" value={Data.From} onChange={(e) => changeHander(e)}/>
+          </Form.Item>
+  
+  
+          <Form.Item label="To">
+          <Input type="text" name="To" maxLength="3" placeholder="3 letters" value={Data.To} onChange={(e) => changeHander(e)}/>
+          </Form.Item>
+          
+          
+  
+          <Form.Item label="Flight Date">
+          <DatePicker type="date" format="DD-MM-YYYY" value={Data.Flight_Date} format="DD-MM-YYYY, HH:mm"
+          showTime="true" disabledDate={d => d.isBefore(new Date())}
+             name="FlightDate" onChange={(date) => setState(prevData => {
+                return {...prevData ,Flight_Date: date}}) 
+      }/>
+          </Form.Item>
+  
+  
+          <Form.Item label="Terminal">
+          <Select type="text" name="Terminal" value={Data.Terminal} onSelect={(value) => setState(prevData => {
+    return {...prevData ,Terminal: value}})}>
+            <Select.Option value="1">Terminal 1</Select.Option>
+            <Select.Option value="2">Terminal 2</Select.Option>
+            <Select.Option value="3">Terminal 3</Select.Option>
+          </Select>
+        </Form.Item>
+  
+          <Form.Item label="Economy Seats"> 
+          <InputNumber type="Number" name="Economy_Seats" value={Data.Economy_Seats} max={500} placeholder="500 Max" onChange={(number) => setState(prevData => {
+              return {...prevData ,Economy_Seats: number}}) 
+          }/>
+        </Form.Item>
+
+        <Form.Item label="Business Seats"> 
+          <InputNumber type="Number" name="Business_Seats" value={Data.Business_Seats} max={500} placeholder="500 Max" onChange={(number) => setState(prevData => {
+              return {...prevData ,Business_Seats: number}}) 
+          }/>
+        </Form.Item>
+
+        <Form.Item label="First Seats"> 
+          <InputNumber type="Number" name="First_Seats" value={Data.First_Seats} max={500} placeholder="500 Max" onChange={(number) => setState(prevData => {
+              return {...prevData ,First_Seats: number}}) 
+          }/>
+        </Form.Item>
+  
+    
+          <Form.Item>
+           <Button onClick={(e) => searchHandler(e)} >Search</Button>
+          </Form.Item>
+  
+        </Form>
+      </>
+      
+    );
+    
+
+  }
+
+
+  return (
+    <>
+      <Form
+        labelCol={{
+          span: 4,
+        }}
+        wrapperCol={{
+          span: 14,
+        }}
+        layout="horizontal"
+        initialValues={{
+          size: componentSize,
+        }}
+        onValuesChange={onFormLayoutChange}
+        size={componentSize}
+      >
+  
+  <Form.Item label="Flight_No:">
+          <Input type="text" name="Flight_No" maxLength="5" placeholder="FNXXX" value={Data.Flight_No} onChange={(e) => changeHander(e)}/>
+        </Form.Item>
+    
+          <Form.Item label="From">
+            <Input type="text" name="From" maxLength="3" placeholder="3 letters" value={Data.From} onChange={(e) => changeHander(e)}/>
+          </Form.Item>
+  
+  
+          <Form.Item label="To">
+          <Input type="text" name="To" maxLength="3" placeholder="3 letters" value={Data.To} onChange={(e) => changeHander(e)}/>
+          </Form.Item>
+          
+          
+  
+          <Form.Item label="Flight Date">
+          <DatePicker type="date" format="DD-MM-YYYY" value={Data.Flight_Date} format="DD-MM-YYYY, HH:mm"
+          showTime="true" disabledDate={d => d.isBefore(new Date())}
+             name="FlightDate" onChange={(date) => setState(prevData => {
+                return {...prevData ,Flight_Date: date}}) 
+      }/>
+          </Form.Item>
+  
+  
+          <Form.Item label="Terminal">
+          <Select type="text" name="Terminal" value={Data.Terminal} onSelect={(value) => setState(prevData => {
+    return {...prevData ,Terminal: value}})}>
+            <Select.Option value="1">Terminal 1</Select.Option>
+            <Select.Option value="2">Terminal 2</Select.Option>
+            <Select.Option value="3">Terminal 3</Select.Option>
+          </Select>
+        </Form.Item>
+  
+          <Form.Item label="Economy Seats"> 
+          <InputNumber type="Number" name="Economy_Seats" value={Data.Economy_Seats} max={500} placeholder="500 Max" onChange={(number) => setState(prevData => {
+              return {...prevData ,Economy_Seats: number}}) 
+          }/>
+        </Form.Item>
+
+        <Form.Item label="Business Seats"> 
+          <InputNumber type="Number" name="Business_Seats" value={Data.Business_Seats} max={500} placeholder="500 Max" onChange={(number) => setState(prevData => {
+              return {...prevData ,Business_Seats: number}}) 
+          }/>
+        </Form.Item>
+
+        <Form.Item label="First Seats"> 
+          <InputNumber type="Number" name="First_Seats" value={Data.First_Seats} max={500} placeholder="500 Max" onChange={(number) => setState(prevData => {
+              return {...prevData ,First_Seats: number}}) 
+          }/>
+        </Form.Item>
+  
+    
+          <Form.Item>
+           <Button onClick={(e) => searchHandler(e)} >Search</Button>
+          </Form.Item>
+
+
+        <div className="">
+      <div className="content">
+        
+          <h1>Search Results </h1>
+
+          <br/>
+          
+            
+          {Result.map(flight =>
+          <div className="row">
+          <p className="left-txt"> <b>Flight_No: </b> {flight.Flight_No} </p>
+         <p className="left-txt"> <b>From: </b> {flight.From} </p>
+         <p className="left-txt"> <b>To: </b> {flight.To} </p>
+          <p className="left-txt"> <b>Flight Date: </b> {moment(flight.Flight_Date).format("YYYY-MM-DD")} </p>
+         <p className="left-txt"> <b>Departure: </b> {moment(flight.Flight_Date).format("HH:mm")} </p>
+         <p className="left-txt"> <b>Terminal: </b> {flight.Terminal} </p>
+         <p className="left-txt"> <b>Economy Class: </b> {flight.Economy_Seats} </p>
+         <p className="left-txt"> <b>Business Class: </b> {flight.Business_Seats} </p>
+         <p className="left-txt"> <b>First Class: </b> {flight.First_Seats} </p>
+          </div>
+     
+            )}
+          
+      </div>
+    </div>
+
+      </Form>
+    </>
+  );
+};
+export default SearchFlight;
